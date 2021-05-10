8185bab6aa44ab4333676fb394401db3a78e5cf7
Vincent Yu <vinceyu1228@gmail.com>
2021-05-11 00:05:04 +0800

    setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   # pens
+  # resources :pens, only: [:index, :new], path_names: { index: 'your-work', new: 'pen' }
+  
+  get '/your-work', to: 'pens#index', as: 'pens'
+  get '/pen', to: 'pens#new', as: 'new_pen'
+
   scope ':username' do
-    resources :pens, except: [:index]
+    resources :pens, except: [:index, :new]
   end
-  get '/your-work', to: 'pens#index'
 
   # static pages
   root 'statics#index'

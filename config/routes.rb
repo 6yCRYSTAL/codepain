5102a4cb41dd4eb043cceba46072e9f2b5118db8
John <cyhsu.john@gmail.com>
2021-05-11 19:06:35 +0800

    route modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: 'accounts'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+  
 
   # pens
   # resources :pens, only: [:index, :new], path_names: { index: 'your-work', new: 'pen' }

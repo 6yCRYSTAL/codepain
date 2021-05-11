3a23888c66eb9073fe8429f027aa5ec87d18e30f
Vincent Yu <vinceyu1228@gmail.com>
2021-05-11 22:34:41 +0800

    fixed Gemfile.lock conflict

--- app/javascript/editor/editor_save_button.js
+++ app/javascript/editor/editor_save_button.js
@@ -1,31 +1,5 @@
 import Rails from '@rails/ujs'
 
 document.addEventListener('turbolinks:load', () => {
-  let saveBtn = document.querySelector('#save-btn')
-  let title = document.querySelector('#title').textContent
-  let userName = document.querySelector('#user-name').textContent
-  let html = document.querySelector('#user-input-html').textContent
-  let css = document.querySelector('#user-input-css').textContent
-  let js = document.querySelector('#user-input-js').textContent
-
-  saveBtn.addEventListener('click', () => {
-    Rails.ajax({
-      url: `/pen`,
-      type: 'POST',
-      dataType: 'json',
-      data: {
-        // params: {
-          title: 'TTT',
-          html: 'HHH'
-        // }
-      },
-      success: response => {
-        console.log('response')
-      },
-      error: err => {
-        console.log('shit')
-      }
-    })
-
-  })
+ 
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  # users
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # pens
   get '/your-work', to: 'pens#index', as: 'pens'
   get '/pen', to: 'pens#new', as: 'new_pen'
   
   # wait for devise user
-  # scope 'username' do
+  scope 'username' do
     resources :pen, controller: 'pens', except: [:index, :new], param: :random_url
-  # end
+  end
 
   # static pages
   root 'statics#index'

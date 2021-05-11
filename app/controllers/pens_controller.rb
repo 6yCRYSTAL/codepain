9842561cdc3e69c57e6a8ff84e974a0a9d42b1a0
yenting <bluejack.ac96@gmail.com>
2021-05-11 17:02:56 +0800

    add seeds.rb for pen data/ render pen#index

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -1,5 +1,7 @@
 class PensController < ApplicationController
-
+  # before_action :authenticate_user!, except: [:new] 
+  # after User modle complete
+  
   def index
     @pens = Pen.all
   end
@@ -11,6 +13,25 @@ class PensController < ApplicationController
   def create
     # 看看要不要丟一包資料給前端
     # pen_path(@pen) 現在有random_url
+  end
+
+  def show
+    render 'pen#show'
+  end
+
+  def update
+    if @pen.update(pen_params)
+      flash[:notice] = 'Pen saved.'
+      #redirect_to restaurant_path(@restaurant), notice: '成功修改資料'
+    else
+      #render :edit
+    end
+
+  end
+
+  private  
+  def pen_params
+    params.require(:pen).permit(:title, :tel, :address, :email, :description)
   end
 
 end

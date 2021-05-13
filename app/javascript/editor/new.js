65c6e3d1817ecc37f0250262275a7d7d1863bb0d
p4952015 <p4952015@yahoo.com.tw>
2021-05-13 13:16:27 +0800

    finish fun console not yet

--- app/javascript/editor/new.js
+++ app/javascript/editor/new.js
@@ -14,94 +14,134 @@ document.addEventListener('DOMContentLoaded', () => {
   let editorCSS = ace.edit("editor--css")
   let editorJS = ace.edit("editor--js")
 
-
-    let init = () => {
-      //set options
-      editorHTML.setOptions({
-        mode: "ace/mode/html",
-        theme: "ace/theme/twilight",
-        highlightActiveLine: true,
-        fontFamily: 'monospace',
-        fontSize: '12pt',
-        tabSize: '2',
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-      })  
-      editorCSS.setOptions({
-        mode: "ace/mode/css",
-        theme: "ace/theme/twilight",
-        highlightActiveLine: true,
-        fontFamily: 'monospace',
-        fontSize: '12pt',
-        tabSize: '2',
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-      })
-      editorJS.setOptions({
-        mode: "ace/mode/javascript",
-        theme: "ace/theme/twilight",
-        highlightActiveLine: true,
-        fontFamily: 'monospace',
-        fontSize: '12pt',
-        tabSize: '2',
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-      })
-      editorHTML.getSession().on('change',function(){
-        renderToiframe()
-      })
-      editorCSS.getSession().on('change',function(){
-        renderToiframe()
-      })
-      editorJS.getSession().on('change',function(){
-        renderToiframe()
-      })
-    }
-
-  //event
-  //get console 
-  // function getSessionValue(){
-  //   editorHTML.session.on('change',function(){
-  //     try{
-  //       editorHTML.session.getValue()
-  //       // console.log(editorHTML.session.getValue())
-  //     }catch(err){
-  //       console.error(err)
-  //     }
-  //   })
-  //   editorCSS.session.on('change',function(){
-  //     try{
-  //       editorCSS.session.getValue()
-  //       // console.log(editorCSS.session.getValue())
-  //     }catch(err){
-  //       console.error(err)
-  //     }
-  //   })
-  //   editorJS.session.on('change',function(){
-  //     try{
-  //       editorJS.session.getValue()
-  //       // console.log(editorJS.session.getValue())
-  //     }catch(err){
-  //       console.error(err)
-  //     }
-  //   })
-  // }
-  //render to iframe
-
-
-  let renderToiframe  = () => {
-    // let result = document.querySelector('#edit--result')
-    let result = document.querySelector('#edit--result').contentDocument
-    result.open()
-    result.write(`${editorHTML.getValue()}`)
-    result.write(`<style>${editorCSS.getValue()}</style>`)
-    result.write(`<script>${editorJS.getValue()}</script>`)
-    result.close()
+  let init = () => {
+    //set options
+    editorHTML.setOptions({
+      mode: "ace/mode/html",
+      theme: "ace/theme/twilight",
+      highlightActiveLine: true,
+      fontFamily: 'monospace',
+      fontSize: '12pt',
+      tabSize: '2',
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: true,
+    })  
+    editorCSS.setOptions({
+      mode: "ace/mode/css",
+      theme: "ace/theme/twilight",
+      highlightActiveLine: true,
+      fontFamily: 'monospace',
+      fontSize: '12pt',
+      tabSize: '2',
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: true,
+    })
+    editorJS.setOptions({
+      mode: "ace/mode/javascript",
+      theme: "ace/theme/twilight",
+      highlightActiveLine: true,
+      fontFamily: 'monospace',
+      fontSize: '12pt',
+      tabSize: '2',
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: true,
+    })
+    editorHTML.getSession().on('change',function(){
+      renderToiframe()
+    })
+    editorCSS.getSession().on('change',function(){
+      renderToiframe()
+    })
+    editorJS.getSession().on('change',function(){
+      renderToiframe()
+    })
   }
 
+  //TODO postMessage
+  //render to iframe
+  let renderToiframe  = () => {
+    let result = document.querySelector('#edit--result').contentDocument
+    result.open()
+    result.write(`${editorHTML.session.getValue()}`)
+    result.write(`<style>${editorCSS.session.getValue()}</style>`)
+    result.write(`<script>${editorJS.session.getValue()}</script>`)
+    result.close()
+  }
+  //TODO
+
+  // get console 
+  let consoleLogList = document.querySelector('.edit-console-ul')
+  let consoleMessage = []
+
+  let printToConsole = () => {
+    consoleMessage.forEach(log => {
+      const newLogItem = document.createElement('li')
+      const newLogText = document.createElement('p')
+      // const newLogText = document.createElement('pre')
+  
+      newLogItem.setAttribute('class', log.class)
+      newLogText.textContent = `> ${log.message}`
+
+      newLogItem.appendChild(newLogText)
+      consoleLogList.appendChild(newLogItem)
+    })
+  }
+
+  editorHTML.session.on('chane', () => {
+    //get user insert code
+    const userHTMLCode = editorHTML.getValue()
+
+    //run in console
+    try{
+      userHTMLCode
+    }catch(errMessage){
+      console.error(errMessage);
+    }
+  })
+
+  editorCSS.session.on('chane', () => {
+    //get user insert code
+    const userCSSCode = editorCSS.getValue()
+
+    //run in console
+    try{
+      userCSSCode
+    }catch(errMessage){
+      console.error(errMessage);
+    }
+  })
+
+  editorJS.session.on('chane', () => {
+    //get user insert code
+    const userJSCode = editorJS.getValue()
+
+    //run in console
+    try{
+      userJSCode
+    }catch(errMessage){
+      console.error(errMessage);
+    }
+  }) 
+
+  let clearConsoleScreen = () => {
+    consoleMessage.length = 0
+
+    while (consoleLogList.firstChild) {
+      consoleLogList.removeChild(consoleLogList.firstChild)
+    }
+
+  }
+  let clearBtn = document.querySelector('#clear-console-btn')
+  clearBtn.addEventListener('click', () => {
+    clearConsoleScreen()
+  })
+
+
+
+  //get web url
   document.querySelector('#edit-share-btn').addEventListener('click', () => {
     alert(location.href)
   })
@@ -109,9 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   init()
   renderToiframe()
-  // getSessionValue()
-
-
+  printToConsole()
 
 })
 

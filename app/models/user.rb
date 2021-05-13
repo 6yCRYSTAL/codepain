846cf1394f21d2d37b419496d2474c5be2058b21
John <cyhsu.john@gmail.com>
2021-05-13 19:48:23 +0800

    combine doubled method, remove testing class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
 
-  def self.from_omniauth_google(auth)
+  def self.from_omniauth_provider(auth)
     data = auth.info
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = data.email
       user.password = Devise.friendly_token[0, 20]
       user.username = data.name
       user.display_name = data.name
-      user.skip_confirmation!
-    end
-  end
-
-  def self.from_omniauth_github(auth)
-    data = auth.info
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = data.email
-      user.password = Devise.friendly_token[0, 20]
-      user.username = data.name
-      user.display_name = data.name
-      # user.image = auth.info.image  / assuming the user model has an image
       user.skip_confirmation!
     end
   end

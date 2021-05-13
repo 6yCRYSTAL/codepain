79a0587265ef058bd13ca4ca52ae328635ee6a59
p4952015 <p4952015@yahoo.com.tw>
2021-05-13 19:24:54 +0800

    tmp

--- app/javascript/editor/new.js
+++ app/javascript/editor/new.js
@@ -72,76 +72,29 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   //TODO
 
+
+  //TODO
   // get console 
-  let consoleLogList = document.querySelector('.edit-console-ul')
+  const consoleResult = document.querySelector('.edit-console')
+  const editConsole = document.querySelector('#console-btn')
+
+  let windowConsole = console.log
+  // let windowConsole = console.log.bind(console)
   let consoleMessage = []
-
-  let printToConsole = () => {
-    consoleMessage.forEach(log => {
-      const newLogItem = document.createElement('li')
-      const newLogText = document.createElement('p')
-      // const newLogText = document.createElement('pre')
   
-      newLogItem.setAttribute('class', log.class)
-      newLogText.textContent = `> ${log.message}`
+  editConsole.addEventListener('click', ()=>{
+    windowConsole.apply(console, arguments)
+    consoleMessage.push.apply(consoleMessage, arguments)
 
-      newLogItem.appendChild(newLogText)
-      consoleLogList.appendChild(newLogItem)
-    })
-  }
-
-  editorHTML.session.on('chane', () => {
-    //get user insert code
-    const userHTMLCode = editorHTML.getValue()
-
-    //run in console
-    try{
-      userHTMLCode
-    }catch(errMessage){
-      console.error(errMessage);
-    }
+    console.log(consoleMessage)
+    consoleResult.innerHTML = consoleMessage
   })
-
-  editorCSS.session.on('chane', () => {
-    //get user insert code
-    const userCSSCode = editorCSS.getValue()
-
-    //run in console
-    try{
-      userCSSCode
-    }catch(errMessage){
-      console.error(errMessage);
-    }
-  })
-
-  editorJS.session.on('chane', () => {
-    //get user insert code
-    const userJSCode = editorJS.getValue()
-
-    //run in console
-    try{
-      userJSCode
-    }catch(errMessage){
-      console.error(errMessage);
-    }
-  }) 
-
-  let clearConsoleScreen = () => {
-    consoleMessage.length = 0
-
-    while (consoleLogList.firstChild) {
-      consoleLogList.removeChild(consoleLogList.firstChild)
-    }
-
-  }
-  let clearBtn = document.querySelector('#clear-console-btn')
-  clearBtn.addEventListener('click', () => {
-    clearConsoleScreen()
-  })
+  //TODO
 
 
 
-  //get web url
+
+  //share btn get web url
   document.querySelector('#edit-share-btn').addEventListener('click', () => {
     alert(location.href)
   })
@@ -149,7 +102,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   init()
   renderToiframe()
-  printToConsole()
 
 })
 

727b0c5dda5dc6a4bb9bde02a57f400b29b56295
John <cyhsu.john@gmail.com>
2021-05-14 14:41:53 +0800

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ require 'date'
   user = User.new(
     email: Faker::Internet.free_email,
     password: "password",
-    username: Faker::Internet.first_name,
+    username: Faker::Name.first_name,
     display_name: Faker::Name.first_name
   )
   user.save

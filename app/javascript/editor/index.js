d8ee24013980d772c3abb0733fb74d32f35ce086
p4952015 <p4952015@yahoo.com.tw>
2021-05-17 14:29:28 +0800

    edit.html畫面出來了

--- app/javascript/editor/index.js
+++ app/javascript/editor/index.js
@@ -1,7 +1,7 @@
 // import your editor js here
 import './new'
-// import './editor_save_button'
-// import './editor_delete_button'
-// import './editor_show_button'
-// import './editor_restore_or_del_button'
+import './editor_save_button'
+import './editor_delete_button'
+import './editor_show_button'
+import './editor_restore_or_del_button'
 

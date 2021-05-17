5ce306b68fd3e7ebc065f873b1960ad8530a9d42
p4952015 <p4952015@yahoo.com.tw>
2021-05-17 16:45:37 +0800

    add patch package to fix node_modules modify file

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 railsweb: bin/rails server -p 3000
 webpack: bin/webpack-dev-server
+postinstall: patch-package
--- app/javascript/editor/index.js
+++ app/javascript/editor/index.js
@@ -4,4 +4,3 @@ import './editor_save_button'
 import './editor_delete_button'
 import './editor_show_button'
 import './editor_restore_or_del_button'
-

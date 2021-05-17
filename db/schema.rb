1fe7c95baf8da4a85596a5effb37a580f71a1980
Vincent Yu <vinceyu1228@gmail.com>
2021-05-17 19:07:38 +0800

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2021_05_17_065613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "pen_id", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["deleted_at"], name: "index_comments_on_deleted_at"
+    t.index ["pen_id"], name: "index_comments_on_pen_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "heart_lists", force: :cascade do |t|
     t.bigint "pen_id", null: false
     t.bigint "user_id", null: false
@@ -64,6 +76,8 @@ ActiveRecord::Schema.define(version: 2021_05_17_065613) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "comments", "pens"
+  add_foreign_key "comments", "users"
   add_foreign_key "heart_lists", "pens"
   add_foreign_key "heart_lists", "users"
   add_foreign_key "pens", "users"

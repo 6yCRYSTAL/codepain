7fc9426137e71befc4beaa41b9620274e7212ed1
John <cyhsu.john@gmail.com>
2021-05-19 11:14:28 +0800

    wip

---
+++ app/javascript/frontend/fontawesomebrands.js
@@ -0,0 +1,16 @@
+import { library, dom } from '@fortawesome/free-brands-svg-icons'
+
+ import { 
+  faFacebook,
+  faGithub,
+  faGoogle
+ } from '@fortawesome/free-brands-svg-icons'
+
+
+
+library.add(
+  faFacebook,
+  faGithub,
+  faGoogle
+  )
+dom.watch()

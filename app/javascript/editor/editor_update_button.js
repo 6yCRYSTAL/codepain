36d7859297dda0e6538cb9eab1e0a92deff0a3dc
p4952015 <p4952015@yahoo.com.tw>
2021-05-19 16:03:34 +0800

    update fn

--- app/javascript/editor/editor_update_button.js
+++ app/javascript/editor/editor_update_button.js
@@ -1,70 +1,32 @@
 import Rails from '@rails/ujs'
 
 document.addEventListener('turbolinks:load', () => {
-  const saveBtn = document.querySelector('#btn-save');
-  const editBtn = document.querySelector('#btn-edit');
-  const input = document.querySelector('#inputTitle');
-  const title = document.querySelector('#edit-title');
+  let updateBtn = document.querySelector('#btn-update')
+  let title = document.querySelector('#edit-title')
+  let username = document.querySelector('#username').textContent
+  let html = ace.edit("editor--html")
+  let css = ace.edit("editor--css")
+  let js = ace.edit("editor--js")
 
-  // edit 編輯和存取
-  if (editBtn && saveBtn){
-    editBtn.addEventListener('click', () => {
-      title.textContent= '';
-      title.style.display="none";
-      input.style.display="inline";
-      editBtn.style.display="none";
-      input.focus();
-    })
-    let allEvent = 0;
-    input.addEventListener('keyup', (e) => {
-      allEvent =  allEvent + 1;
-      if(e.keyCode === 13 && (allEvent !== 3)){
-        eventContent(e);
-      }else{
-        allEvent = 0;
-      }
-    })
-    input.addEventListener('blur', (e) => {
-      allEvent = allEvent + 2;
-      if(allEvent !== 3){
-        eventContent(e);
-      }else{
-        allEvent = 0;
-      }
-    })
-    let eventContent = (e)=>{
-      title.style.display="inline";
-      input.style.display="none";
-      editBtn.style.display="inline";
-      let inputValue = e.target.value;
-      // console.log(inputValue);
+  updateBtn.addEventListener('click', () => {
+    let titleNew = title.textContent
+    let htmlValue = html.session.getValue()
+    let cssValue = css.session.getValue()
+    let jsValue = js.session.getValue()
+    let randomurl = location.href.split('pen/')[1]
+    let paramsFromNewPen = () => {
+      return `user[username]=${username}&pen[title]=${titleNew}&pen[html]=${htmlValue}&pen[css]=${cssValue}&pen[js]=${jsValue}`
     }
-    
-    let titleNew = inputValue
-    let username = document.querySelector('#username').textContent
-    let html = ace.edit("editor--html")
-    let css = ace.edit("editor--css")
-    let js = ace.edit("editor--js")
-
-    saveBtn.addEventListener('click', () => {
-      let htmlValue = html.session.getValue()
-      let cssValue = css.session.getValue()
-      let jsValue = js.session.getValue()
-      let randomurl = location.href.split('pen/')[1]
-      let paramsFromNewPen = () => {
-        return `user[username]=${username}&pen[title]=${titleNew}&pen[html]=${htmlValue}&pen[css]=${cssValue}&pen[js]=${jsValue}`
-      }
-    
-      Rails.ajax({
-        url: `'/api/v1/pens/${randomurl}'`,
-        type: 'POST',
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-        },
-        dataType: 'json',
-        data: paramsFromNewPen()
-      })
+  
+    Rails.ajax({
+      url: `/api/v1/pens/${randomurl}`,
+      type: 'PUT',
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      dataType: 'json',
+      data: paramsFromNewPen()
     })
-  }
+  })
 })

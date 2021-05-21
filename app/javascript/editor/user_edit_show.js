f7f0300e186e36b703a131e3f0cc7f533a2d5632
p4952015 <p4952015@yahoo.com.tw>
2021-05-21 15:08:48 +0800

    rename user_edit_show

---
+++ app/javascript/editor/user_edit_show.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+document.addEventListener('turbolinks:load', () => {
+  const html = document.querySelector('#editor--html')
+  const css = document.querySelector('#editor--css')
+  const js = document.querySelector('#editor--js')
+  
+  if(html && css && js){
+    let updateBtn = document.querySelector('#btn-update')
+    let randomurl = location.href.split('/pen/')[1]
+    let title = document.querySelector('#edit-title')
+
+    let editorHTML = ace.edit("editor--html")
+    let editorCSS = ace.edit("editor--css")
+    let editorJS = ace.edit("editor--js")
+
+    if (updateBtn){
+      axios({
+        method: 'get',
+        url: `/api/v1/pens/${randomurl}/edit`
+      })
+      .then( (response) => {
+          let data = response.data
+          title.textContent = data.title
+          editorHTML.session.setValue(data.html)
+          editorCSS.session.setValue(data.css)
+          editorJS.session.setValue(data.js)
+        })
+        .catch( (error) => {
+          return `${error.name}: ${error.message}`
+        })
+    }
+  }
+})

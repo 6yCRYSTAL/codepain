2df8d559d7493f648b1a4b724e81e72060ccef3b
yenting <bluejack.ac96@gmail.com>
2021-05-23 02:01:29 +0800

    add api/v1/comments#creat and comment create function by ajax (WIP)

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -2,20 +2,36 @@ import { Controller } from "stimulus"
 import Rails from '@rails/ujs'
 export default class extends Controller {
 
-  static targets = [ "editBtn", "cancelBtn", "commentBlock", "textarea", "commentShow", "updateBtn"]
+  static targets = ["createBtn", "editBtn", "cancelBtn", "commentBlock", "updateTextArea","createTextArea", "commentShow", "updateBtn"]
 
   initialize() {
     this.comment_id = ''
   }
 
+  create() {
+    let content = this.createTextAreaTarget.value
+    let randomurl = location.href.split('/').reverse()[0]
+    Rails.ajax({
+      url: `/api/v1/comments`,
+      type: 'POST',
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      dataType: 'json',
+      data: `content=${content}&random_url=${randomurl}`
+    })
+    // 取回後端資料渲染
+  }
+
   edit() {
     this.commentBlockTarget.classList.add("appear")
-    this.textareaTarget.value = this.commentShowTarget.textContent
+    this.updateTextAreaTarget.value = this.commentShowTarget.textContent
     this.comment_id = this.editBtnTarget.dataset.id
   }
 
   update() {
-    let newContent = this.textareaTarget.value
+    let newContent = this.updateTextAreaTarget.value
     this.commentShowTarget.textContent = newContent
     let content_upate = () => {
       return `content=${newContent}`

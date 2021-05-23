57e6f1bdcd8a48cf7ca42f0be134544d01392deb
Mandy Tsai <mandy3152618@gmail.com>
2021-05-23 15:53:42 +0800

    fix/edit-title-name

--- app/javascript/editor/edit_title.js
+++ app/javascript/editor/edit_title.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import getSaveBtn from './save_button.js'
 
 document.addEventListener('turbolinks:load', () => {
-  const editBtn = document.querySelector('#btn-edit');
+  const editTitleBtn = document.querySelector('#btn-edit');
   const input = document.querySelector('#inputTitle');
   const title = document.querySelector('#edit-title');
   let randomURL = location.href.split('/pen/')[1];
@@ -13,11 +13,11 @@ document.addEventListener('turbolinks:load', () => {
   ax.defaults.headers.common['X-CSRF-Token'] = token;
 
   // edit 編輯和存取
-  if (editBtn){
-    editBtn.addEventListener('click', () => {
+  if (editTitleBtn){
+    editTitleBtn.addEventListener('click', () => {
       title.style.display="none";
       input.style.display="inline";
-      editBtn.style.display="none";
+      editTitleBtn.style.display="none";
       input.focus();
     })
 
@@ -43,7 +43,7 @@ document.addEventListener('turbolinks:load', () => {
     let eventContent = (e) => {
       title.style.display="inline";
       input.style.display="none";
-      editBtn.style.display="inline";
+      editTitleBtn.style.display="inline";
       inputValue = e.target.value;
       title.textContent = inputValue;
       if (input.value === ""){
@@ -61,7 +61,10 @@ document.addEventListener('turbolinks:load', () => {
       let newTitle = inputValue;
       ax.patch(`/api/v1/pens/${randomURL}`,{ pen: { title: newTitle }})
       .then(res =>{
+        // TODO 黃色標籤未做
         console.log(res.data.status);
+        if(res.data.status === 'update succeeded'){
+        }
       }).catch(error => { 
         // console.log(error.status) 
         console.log('error');

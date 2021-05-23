6132fcd24a4a5e5a33284812df8274290266526d
Senri <z22041161@gmail.com>
2021-05-24 07:17:35 +0800

    0524pin_arrow

--- app/javascript/frontend/pin.js
+++ app/javascript/frontend/pin.js
@@ -1,6 +1,7 @@
 document.addEventListener('turbolinks:load', () => {
   document.querySelector('.btn-pinned').addEventListener('click', function(){
     // console.log('123')
+    
     // document.querySelector('.btn-pinned').classList.toggle('')
     // btn-pinned.classList.toggle('')
   })

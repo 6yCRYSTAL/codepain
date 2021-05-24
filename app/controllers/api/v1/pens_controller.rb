a2eb96db3316f4404e6898ced62dea864f0e2b9c
yenting <bluejack.ac96@gmail.com>
2021-05-24 23:59:17 +0800

    create pin index (api/v1/pens#pin_list) and create (api/v1/pens#pin_create) api

--- app/controllers/api/v1/pens_controller.rb
+++ app/controllers/api/v1/pens_controller.rb
@@ -44,6 +44,27 @@ class Api::V1::PensController < Api::ApiController
     end
   end
 
+  def pin_list
+    pin_list = current_user.pined_pens.select(:title, :updated_at, :id, :user_id).reverse
+    # 繞路做法，為了排序pin及查出每個被pin的pen的作者，有 N + 1 效能問題
+    user_list =[]
+    pin_list.each_with_index do |pin ,i|
+      user_list << User.where("id = ?", pin.user_id).select(:username, :display_name)
+    end
+    success!({ pined_pens: pin_list, user_list: user_list})
+  end
+
+  def pin_create
+    pen = Pen.find_by(random_url: params.permit(:random_url)[:random_url])
+    if current_user.pined?(pen)
+      current_user.pined_pens.destroy(pen)
+      success!({ boolean: pin_pen?(pen) }, 'removed')
+    else
+      current_user.pined_pens << pen
+      success!({ boolean: pin_pen?(pen) }, 'added')
+    end
+  end
+
   def grid
     pens_per_page(params[:page], 6)
 
@@ -74,6 +95,10 @@ class Api::V1::PensController < Api::ApiController
 
   def love_pen?
     HeartList.where('pen_id = ? AND user_id = ?', pen.id, current_user.id).exists?
+  end
+
+  def pin_pen?(pen)
+    Pin.where('pen_id = ? AND user_id = ?', pen.id, current_user.id).exists?
   end
 
   def love_params
--- app/models/pen.rb
+++ app/models/pen.rb
@@ -10,6 +10,7 @@ class Pen < ApplicationRecord
   has_many :heart_list
   has_many :lovers, through: :heart_list, source: :user
   has_many :pins
+  has_many :piners, through: :pins, source: :user
 
   scope :is_trashed, -> { only_deleted.where(state: 'trashed') }
   scope :deleted_in_1_hour, -> { is_trashed.where('deleted_at > ?', 1.hour.ago) }

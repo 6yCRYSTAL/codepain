2b054464af93b2a1745da56dc205552dd6cd989b
Mandy Tsai <41838993+mandy0315@users.noreply.github.com>
2021-05-25 11:41:50 +0800

    Fix/edit title (#123)
    * fix/console bug
    
    * fix/edit-title bug and add api
    
    * fix/edit-title-name
    
    * show yellow frame
    
    * fix/edit-title name
    
    * fix/edit_title.js editTitleBtn-name
    
    * edit-title&save bug & update title bug
    
    * fix/edit-title bug & edit_user_show title name
    
    * add/show-inputValue.value & delete/pen-controller comment
    
    * fix conflict

--- app/javascript/editor/user_edit_show.js
+++ app/javascript/editor/user_edit_show.js
@@ -4,11 +4,12 @@ document.addEventListener('turbolinks:load', () => {
   const html = document.querySelector('#editor--html')
   const css = document.querySelector('#editor--css')
   const js = document.querySelector('#editor--js')
-  
+
   if(html && css && js){
     let updateBtn = document.querySelector('#btn-update')
     let randomurl = location.href.split('/pen/')[1]
     let title = document.querySelector('#edit-title')
+    let inputValue = document.getElementById("input-title")
 
     let editorHTML = ace.edit("editor--html")
     let editorCSS = ace.edit("editor--css")
@@ -23,6 +24,7 @@ document.addEventListener('turbolinks:load', () => {
         let data = response.data
         if(data.status === "ok"){
           title.textContent = data.payload.title
+          inputValue.value = data.payload.title
           editorHTML.session.setValue(data.payload.html)
           editorCSS.session.setValue(data.payload.css)
           editorJS.session.setValue(data.payload.js)

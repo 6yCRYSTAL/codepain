4e6f76b94a4ad984c256a027fd7c0d963dbb37d8
p4952015 <p4952015@yahoo.com.tw>
2021-05-25 12:25:51 +0800

    wip

---
+++ app/javascript/frontend/pin.js
@@ -0,0 +1,11 @@
+document.addEventListener('turbolinks:load', () => {
+  document.querySelector('.btn-pinned').addEventListener('click', function(){
+    const pinBtn= document.querySelector('.btn-pinned')
+    if(pinBtn.classList.contains('btn-pinned-blue')){
+      pinBtn.classList.remove('btn-pinned-blue')
+    }else{
+      pinBtn.classList.add('btn-pinned-blue')
+    }
+  })
+})
+

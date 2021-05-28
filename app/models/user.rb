2d90751a42c5e00cf1aa2e2024f8359c647eb2d4
John <cyhsu.john@gmail.com>
2021-05-28 18:12:33 +0800

    fix pen auth problem, fix user model format, fix email sent locale message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google, :github, :facebook]
+  devise :database_authenticatable,
+         :registerable,
+         :confirmable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :omniauthable,
+           omniauth_providers: [:google, :github, :facebook]
 
   has_many :pens, dependent: :destroy
   has_many :comments

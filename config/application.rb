36e3f7bc4b08145d0aa22c6fc2dcec5164b035b8
John Chaoyung Hsu <65455378+cyhsujohn@users.noreply.github.com>
2021-05-31 17:03:01 +0800

    add clouflare_proxy to lib and require it in config/application (#178)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
-
 require "rails/all"
+require "./lib/cloudflare_proxy"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +10,7 @@ module Codepain
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.middleware.use ::CloudflareProxy
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

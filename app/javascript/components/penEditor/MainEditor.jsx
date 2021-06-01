280fc9396a8fa0e98a3b2555bf3cb6aebda128bd
p4952015 <p4952015@yahoo.com.tw>
2021-06-01 19:27:38 +0800

    console switch done

--- app/javascript/components/penEditor/MainEditor.jsx
+++ app/javascript/components/penEditor/MainEditor.jsx
@@ -8,53 +8,37 @@ import Iframe from './Iframe'
 
 const MainEditor = ( ) => {
 
-  const [showConsole, setShowConsole] = useState(true)
-  const [closeIframe, setCloseIframe] = useState(false)
+  const [showConsole, setShowConsole] = useState(false)
+  const isOpen = showConsole ? "consoleOpen" : "consoleClose"
 
-  const showConsoleBox = () => {
+  function showConsoleBox() {
     setShowConsole(!showConsole)
-    setCloseIframe(!closeIframe)
   }
-  const consoleBtnStlye = {
-    position: "absolute",
-    bottom: "5px",
-    left: "15px",
-    zIndex: "100",
-    color: "var(--light-gray)",
-    fontSize: "var(--text-s-p)",
-    padding: "2px 7px",
-    backgroundColor: "var(--primary-gray-4)",
-    borderRadius: "2px",
-  }
+
 
   return(
     <>
-    <SplitPane split="horizontal" minSize={"50%"} className="SplitPane-horizontal">
-
-      <SplitPane split="vertical" minSize={"33%"}>
-        {/* editor html */}
-        <Editor editorTitle={"HTML"} editorId={"editor--html"} editorClass={"editor-code editor-html"}/>
-        <SplitPane split="vertical" minSize={"50%"}>
-          {/* editor css */}
-          <Editor editorTitle={"CSS"} editorId={"editor--css"} editorClass={"editor-code editor-css"}/>
-          {/* editor js */}
-          <Editor editorTitle={"JavaScript"} editorId={"editor--js"} editorClass={"editor-code editor-js"}/>
+      <SplitPane split="horizontal" minSize={"50%"} className="SplitPane-horizontal">
+        <SplitPane split="vertical" minSize={"33%"}>
+          {/* editor html */}
+          <Editor editorTitle={"HTML"} editorId={"editor--html"} editorClass={"editor-code editor-html"}/>
+          <SplitPane split="vertical" minSize={"50%"}>
+            {/* editor css */}
+            <Editor editorTitle={"CSS"} editorId={"editor--css"} editorClass={"editor-code editor-css"}/>
+            {/* editor js */}
+            <Editor editorTitle={"JavaScript"} editorId={"editor--js"} editorClass={"editor-code editor-js"}/>
+          </ SplitPane>
         </ SplitPane>
+
+        <div className={isOpen}>
+          <SplitPane split="horizontal" minSize={"50%"} className="iframeAndConsole">
+              <Iframe />
+              <EditorConsole />
+          </ SplitPane>
+        </div>
       </ SplitPane>
 
-      {
-        closeIframe && <Iframe />
-      }
-      {
-        showConsole &&
-        <SplitPane split="horizontal" minSize={"50%"}>
-          <Iframe />
-          <EditorConsole />
-        </ SplitPane>
-      }
-    </ SplitPane>
-
-    <button onClick={showConsoleBox} id="console-btn" className="btn-console fake-console" style={consoleBtnStlye}> Console </button>
+      <button onClick={showConsoleBox} id="console-btn" className="fake-console"> Console </button>
     </>
   )
 }

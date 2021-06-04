bf66033463387b6a4a0256beefb5db25fa74e6fa
p4952015 <p4952015@yahoo.com.tw>
2021-06-04 17:43:12 +0800

    fixed grid-comments

--- app/javascript/components/grid/Alert.js
+++ app/javascript/components/grid/Alert.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { comment } from 'postcss';
+import Rails from '@rails/ujs'
 
 // 彈跳視窗功能
 const Alert = (props) => {
@@ -8,6 +7,20 @@ const Alert = (props) => {
   const { user_name,random_url,comments_count,view_count } = props.data;
 
   const [ comments, setComments ] = useState([])
+
+  useEffect( () => {
+    Rails.ajax({
+      type: 'get',
+      url: '/api/v1/comments',
+      data: `random_url=${random_url}`,
+      success: (response) => {
+        //TODO
+        console.log(response.comments)
+        let commentData = response.comments
+        setComments(commentData)
+      }
+    })
+  }, [])
 
   // 關掉彈跳視窗
   function closeAlert(e){
@@ -21,63 +34,50 @@ const Alert = (props) => {
   // 新增網址，而不會刷新頁面
   React.useEffect(() =>{
     history.pushState({user_name, random_url}, `Selected: ${user_name}, ${random_url}`, `./${user_name}/details/${random_url}`);
-  })
-
-  useEffect( () => {
-    axios({
-      method: 'get',
-      url: '/api/v1/comments',
-      params:{
-        random_url: `${random_url}`
-      }
-    })
-    .then( (response) => {
-        //TODO
-        console.log(response.data.payload)
-        let commentData = response.data.payload.comments
-        setComments(commentData)
-    })
-  }, [])
+  },[])
 
   return(
     <div id="modal" className="modal-container" onClick={ closeAlert }>
       <div className="modal-content">
-        <a href="#" id="username"> {user_name} </a>
-        <div className="points-wrap points-content-bottom" data-url={`${random_url}`}></div>
-        <div className="bg-gray-300" data-controller="comment-create">
+        <a href="#" id="username">{ user_name }</a>
+        <div className="points-wrap points-content-bottom" data-url={ random_url }></div>
+        <div className="bg-gray-300"
+             data-controller="comment-create"
+             data-comment-create-url-value={ random_url }
+             data-comment-create-comments-count-value={ comments_count }>
           <section>
             <textarea className="w-6/12 h-20"
                       placeholder={`Want to know how ${user_name} did this? Ask a question!\nFeeling inspired? Let ${user_name} know!\nWant to share how you used this Pen?\nGive the creator a confidence boost!`}
                       data-comment-create-target="createTextArea"></textarea>
-            <span className="bg-gray-500 text-white font-bold py-2 px-4 rounded "
+            <span className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
                   data-action="click->comment-create#create"
                   data-comment-create-target="createBtn">
               Comment
             </span>
           </section>
           <section>
-            <p className="uppercase" data-comments-count={comments_count} data-comment-create-target="commentsCount">
-              {comments_count} comments</p>
-            <ol data-comment-create-target="list" id="comment-list">
+            <p className="uppercase" data-comment-create-target="commentsCount">
+              {comments_count % 2} comments</p>
+            <ol data-comment-create-target="list" id={random_url}>
             {
               comments.map( (commentData) => {
                 return(
                   <li data-controller="comment-update comment-delete"
                       data-comment-update-target="commentLi"
                       data-comment-delete-target="commentLi"
-                      data-comment-delete-id-value={commentData.id}
-                      key={commentData.id}>
+                      data-comment-delete-id-value={ commentData.content.id }
+                      key={ commentData.content.id }>
                     <div>
-                      <span>{user_name} (@{user_name})</span>
-                      <span>{commentData.created_at}</span>
+                      <span>{ commentData.user.display_name } (@{ commentData.user.username })</span>
+                      <span>{ commentData.created_at }</span>
                     </div>
                     <div className="comment-update-block" data-comment-update-target="commentBlock">
-                      <textarea data-comment-update-target="updateTextArea" defaultValue={commentData.content}></textarea>
-                      <button data-action="click->comment-update#update" data-comment-update-target="updateBtn" data-id="{commentData.id}"> Update </button>
+                      <textarea data-comment-update-target="updateTextArea" defaultValue={ commentData.content.content }></textarea>
+                      <button data-action="click->comment-update#update" data-comment-update-target="updateBtn" data-id={ commentData.content.id }> Update </button>
                       <button data-action="click->comment-update#cancel" data-comment-update-target="cancelBtn"> Cancel </button>
                     </div>
                     <div>
-                      <p data-comment-update-target="commentShow">{commentData.content}</p>
+                      <p data-comment-update-target="commentShow">{ commentData.content.content }</p>
                     </div>
                     <button data-action="click->comment-update#edit" data-comment-update-target="editBtn"> Edit </button>
                     <button data-action="click->comment-delete#delete" data-comment-update-target="deleteBtn"> Delete</button>
@@ -89,7 +89,7 @@ const Alert = (props) => {
           </section>
         </div>
         <div>
-          <div id="view_count"><i className="fas fa-eye" /> {view_count} Views</div>
+          <div id="view_count"><i className="fas fa-eye" /> { view_count } Views</div>
         </div>
       </div>
     </div>

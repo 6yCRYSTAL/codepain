1c86e6bfd716db3a1f84847bcba6fecb0f32702c
p4952015 <p4952015@yahoo.com.tw>
2021-06-08 18:19:37 +0800

    修正editor跑版出現出現scrollbar問題

--- app/javascript/components/penEditor/SettingAlertOwner.jsx
+++ app/javascript/components/penEditor/SettingAlertOwner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
-import '../../styles/editor-setting.scss'
+import 'styles/editor-setting.scss'
 import EditorSettingContainerOwner from './settingComponents/EditorSettingContainerOwner'
 
 const SettingAlert = () => {

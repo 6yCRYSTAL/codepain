e4766127e8c0a90f73c5373e9aef377d5c78c90f
Vincent Yu <vinceyu1228@gmail.com>
2021-06-09 00:15:32 +0800

    recommend mobile user using larger device

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,15 @@
 class StaticsController < ApplicationController
+  require "browser"
+  before_action :detect_mobile_user
+
   def index
     redirect_to pens_path if user_signed_in?
+  end
+
+  private
+
+  def detect_mobile_user
+    browser = Browser.new(request.user_agent)
+    render 'mobile' if browser.device.mobile?
   end
 end

9990d47e301ebf2610b29d662898dae97a5dfb17
yenting <bluejack.ac96@gmail.com>
2021-06-08 23:50:53 +0800

    remove require faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
-require 'faker'
 # 10.times do
 #   # password = "password"
 #   user = User.create(

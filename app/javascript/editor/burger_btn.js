c30657fdf7afa2c38ebb2c45994a632cc83c2461
p4952015 <p4952015@yahoo.com.tw>
2021-06-10 19:25:35 +0800

    modify burger btn style

--- app/javascript/editor/burger_btn.js
+++ app/javascript/editor/burger_btn.js
@@ -3,10 +3,21 @@ document.addEventListener('turbolinks:load', () => {
   let headerBtn = document.querySelectorAll('.edit-btn-header > button')
 
   if( burgerBtn && headerBtn){
-    burgerBtn.addEventListener('click', () => {
-      headerBtn.forEach( (btn) => {
-        btn.classList.toggle('open')
-      })
+    // burgerBtn.addEventListener('click', () => {
+    //   headerBtn.forEach( (btn) => {
+    //     btn.classList.toggle('open')
+    //   })
+    // })
+    window.addEventListener('click', function(e){
+      if(burgerBtn.contains(e.target)){
+        headerBtn.forEach( (btn) => {
+          btn.classList.toggle('open')
+        })
+      }else {
+        headerBtn.forEach( (btn) => {
+          btn.classList.remove('open')
+        })
+      }
     })
 
   }

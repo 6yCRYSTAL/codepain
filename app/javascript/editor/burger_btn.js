8ea39bc69b1353c89b2ad938e7fee5bf1799b258
p4952015 <p4952015@yahoo.com.tw>
2021-06-11 10:47:28 +0800

    modify editor burger btn

--- app/javascript/editor/burger_btn.js
+++ app/javascript/editor/burger_btn.js
@@ -2,18 +2,13 @@ document.addEventListener('turbolinks:load', () => {
   let burgerBtn = document.querySelector('.burgerBtn')
   let headerBtn = document.querySelectorAll('.edit-btn-header > button')
 
-  if( burgerBtn && headerBtn){
-    // burgerBtn.addEventListener('click', () => {
-    //   headerBtn.forEach( (btn) => {
-    //     btn.classList.toggle('open')
-    //   })
-    // })
+  if (burgerBtn && headerBtn) {
     window.addEventListener('click', function(e){
-      if(burgerBtn.contains(e.target)){
+      if (burgerBtn.contains(e.target)) {
         headerBtn.forEach( (btn) => {
           btn.classList.toggle('open')
         })
-      }else {
+      } else {
         headerBtn.forEach( (btn) => {
           btn.classList.remove('open')
         })

844ee6afb589a3515148fcfb7a44fcddd23fdfcf
Trishaaa <65930799+p4952015@users.noreply.github.com>
2021-06-14 11:14:42 +0800

    add sweet alert when copied url link (#367)
    * add sweet alert when copied url link
    
    * change pro products tab title
    
    * 修正yourwork中間兩個iframe會出現白邊

--- app/javascript/components/penEditor/ShareUrlBox.jsx
+++ app/javascript/components/penEditor/ShareUrlBox.jsx
@@ -1,4 +1,7 @@
 import React, { useRef } from 'react';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import 'styles/new-edit.scss'
 
 const ShareBox = () => {
 
@@ -8,6 +11,18 @@ const ShareBox = () => {
     const url = refInput.current
     url.select()
     navigator.clipboard.writeText(url.value)
+
+    const MySwal = withReactContent(Swal)
+    MySwal.fire({
+      position: 'top',
+      title: 'Copied Pen URL to clipboard!',
+      showConfirmButton: false,
+      timer: 1500,
+      customClass: {
+        popup: 'copied-link-popup',
+        title: 'copied-link-title'
+      }
+    })
   }
 
   return (

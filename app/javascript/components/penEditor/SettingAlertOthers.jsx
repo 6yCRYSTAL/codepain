764d008f00288dc03854daabe1de4b23c0b16163
Vincent Yu <vinceyu1228@gmail.com>
2021-06-15 00:43:57 +0800

    fix import mistake

--- app/javascript/components/penEditor/SettingAlertOthers.jsx
+++ app/javascript/components/penEditor/SettingAlertOthers.jsx
@@ -3,6 +3,7 @@ import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 import 'styles/editor-setting.scss'
 import EditorSettingContainerOthers from './settingComponents/EditorSettingContainerOthers'
+import Turbolinks from 'turbolinks'
 
 const SettingAlert = () => {
   const MySwal = withReactContent(Swal)

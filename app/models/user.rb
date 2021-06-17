7300fea8c871301b4d13386f8a5ee32974a907d5
Vincent Yu <vinceyu1228@gmail.com>
2021-06-17 17:36:33 +0800

    fixed third party cannot sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :registerable,
-        #  :confirmable,
+         :confirmable,
          :recoverable,
          :rememberable,
          :validatable,

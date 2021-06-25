5bf019c1e29919b55558c46c873608cacbef7e4c
Trishaaa <65930799+p4952015@users.noreply.github.com>
2021-06-25 11:16:58 +0800

    Fix/document.write problem (#425)
    fix document.write problem

--- app/javascript/frontend/gridList_btn.js
+++ app/javascript/frontend/gridList_btn.js
@@ -2,11 +2,15 @@
 export default function GridListStyle() {
   const GridBtn = document.querySelector('.grid-btn button');
   const ListBtn = document.querySelector('.list-btn button');
-  if(location.href.includes('grid_type=list')){
-    ListBtn.style.backgroundColor = "#717790";
-    ListBtn.firstElementChild.style.fill = "#f1f1f3";
-  }else{
-    GridBtn.style.backgroundColor = "#717790";
-    GridBtn.firstElementChild.style.fill = "#f1f1f3";
+
+  if (GridBtn || ListBtn) {
+    if(location.href.includes('grid_type=list')){
+      ListBtn.style.backgroundColor = "#717790";
+      ListBtn.firstElementChild.style.fill = "#f1f1f3";
+    }else{
+      GridBtn.style.backgroundColor = "#717790";
+      GridBtn.firstElementChild.style.fill = "#f1f1f3";
+    }
   }
+
 };
